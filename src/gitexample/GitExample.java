80c050cb1cffdf65aa78d78d013b1577f0fdddc0
mac <mac@localhost>
2026-05-05 10:19:53 +0100

    scanner added

--- src/gitexample/GitExample.java
+++ src/gitexample/GitExample.java
@@ -4,6 +4,8 @@
  */
 package gitexample;
 
+import java.util.Scanner;
+
 /**
  *
  * @author mac
@@ -14,7 +16,8 @@ public class GitExample {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Scanner keyboard = new Scanner(System.in);
+        //CS - salva rapido 
     }
     
 }
